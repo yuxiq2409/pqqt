3eef1bb8d64dc100fed0df46f5b0f3d6c8f830fb
yuxiq2409 <yuxiq2409@163.com>
2019-06-25 04:16:17 +0800

    pyqt1

--- first.py
+++ first.py
@@ -5,19 +5,18 @@ from PyQt5.QtGui import  QIcon
 class FirstMainWin(QMainWindow,untitled.Ui_MainWindow):
    #构造窗体
     def __init__(self,parent=None):
-         super(FirstMainWin,self).__init__(parent)
-         self.setupUi(self) #很重要，是将desiger的控件加到窗体中，由desiger设计的布局
-         self.initUI()
+        super(FirstMainWin,self).__init__(parent)
+        self.setupUi(self) #很重要，是将desiger的控件加到窗体中，由desiger设计的布局
+        self.initUI()
 
-         # 设置信号槽
-         self.pushButton_4.clicked.connect(self.hello)
+        # 设置信号槽
+        self.pushButton_4.clicked.connect(self.hello)
 
-         self.lineEdit_5.textChanged.connect(self.change5)
-         self.lineEdit_14.textChanged.connect(self.change14)
-         self.lineEdit_6.textChanged.connect(self.change6)
-         self.lineEdit_13.textChanged.connect(self.change13)
-
-
+        self.lineEdit_5.textChanged.connect(self.change5)
+        self.lineEdit_14.textChanged.connect(self.change14)
+        self.lineEdit_6.textChanged.connect(self.change6)
+        self.lineEdit_13.textChanged.connect(self.change13)
+        self.pushButton_15.clicked.connect(self.change0)
 
    # 初始化窗体
     def initUI(self):
@@ -28,33 +27,51 @@ class FirstMainWin(QMainWindow,untitled.Ui_MainWindow):
          #设置窗体的标题
          self.setWindowTitle('我的第一个pyqt程序')
 
-
-
-         self.show()
     def hello(self):
         self.textEdit.setText("hello world")
     # def change14(self):
     #
     def change5(self):
-            text14=int(self.lineEdit_5.text())*2
+        if self.lineEdit_5.text() == "":
+            self.lineEdit_14.text = ""
+        else:
+            text14=round(float(self.lineEdit_5.text())*0.74,3)
             self.lineEdit_14.setText(str(text14))
 
     def change14(self):
-            text19 = int(self.lineEdit_14.text()) * 2
+        if self.lineEdit_14.text()== "":
+            self.lineEdit_19.text=""
+        else:
+            text19 =round(float(self.lineEdit_14.text()) * 12,3)
             self.lineEdit_19.setText(str(text19))
 
     def change6(self):
-            text13 = int(self.lineEdit_6.text()) * 2
+        if self.lineEdit_6.text() == "":
+            self.lineEdit_13.text = ""
+        else:
+            text13 = float(self.lineEdit_6.text()) * 2.5
             self.lineEdit_13.setText(str(text13))
+
     def change13(self):
-            text18 = int(self.lineEdit_13.text()) * 2
+        if self.lineEdit_13.text() == "":
+            self.lineEdit_18.text = ""
+        else:
+            text18 = float(self.lineEdit_13.text()) * 2
             self.lineEdit_18.setText(str(text18))
+    def change0(self):
+        self.lineEdit_19.clear()
+        self.lineEdit_14.clear()
+        self.lineEdit_5.clear()
+
+        self.lineEdit_18.clear()
+        self.lineEdit_13.clear()
+        self.lineEdit_6.clear()
 
 
 if __name__=='__main__':
     app=QApplication(sys.argv)
     #实例化窗体
     w=FirstMainWin()
-
+    w.show()
     #打开主循环，并关闭窗体
     sys.exit(app.exec_())
